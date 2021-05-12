419f805ad41524946d460a0e20223182386d4235
Ankita-Dake <ankitadake1997@gmail.com>
2021-05-12 22:24:42 +0530

    first commit

---
+++ PythonInput.py
@@ -0,0 +1,35 @@
+# input from user
+# a = input("Enter value")
+# print(a)
+
+# addition of two number
+# a = int(input("Enter value for a"))
+# b = int(input("Enter value for b"))
+# addition = a + b
+# print('sum of a & b is', addition)
+
+# Boolean
+# print(10<43)
+# print(32>67)
+# print(10==10)
+
+# operator
+# i = 10
+# j = 20
+# sum = i+j
+# print('sum of {0} and {1} is {2}' , sum)
+
+i = 'Hello'
+print(i*2)
+
+i = 10
+i += 10
+print(i)
+
+i = [10,20,30,40,50]
+if 10 in i:
+    print('value match')
+
+j = [1,2,3,4,5,6]
+if 7 not in j:
+    print('value not match ')
